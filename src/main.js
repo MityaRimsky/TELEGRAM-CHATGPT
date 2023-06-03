67c38b0ed0b2356d9c527c880167600732a26b1f
MityaRimsky <123651254+MityaRimsky@users.noreply.github.com>
2023-06-03 17:29:20 +0000

    1

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
-import {Telegraf} from 'telegraf'
+import { Telegraf } from 'telegraf'
+
+const bot = new Telegraf
+
+bot.launch()
+
+process.once ('SIGINT',() => bot.stop('SIGINT'))
+process.once ('SIGTERM',() => bot.stop('SIGTERM'))
