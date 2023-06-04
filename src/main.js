cce42e39eb503a016caca9663f7aa25326bea979
MityaRimsky <123651254+MityaRimsky@users.noreply.github.com>
2023-06-04 14:26:34 +0000

    1

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { Telegraf } from 'telegraf'
 import { message } from 'telegraf/filters'
+import { code } from 'telegraf/format'
 import config from 'config'
 import { ogg } from './ogg.js'
 import { openai } from './openai.js'
@@ -8,6 +9,7 @@ const bot = new Telegraf(config.get('TELEGRAM_TOKEN'))
 
 bot.on(message('voice'), async (ctx) => {
   try {
+    await ctx.reply(code('Ожидайте...'))
     const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id)
     const userId = String(ctx.message.from.id)
     const oggPath = await ogg.create(link.href, userId)
